efb1feeb0214e38357cadf6967584444368ae91c
griminir <viktor.elliot.king@gmail.com>
2025-01-24 11:42:55 +0100

    added tailwind, grabbed the tailwind theme form tutorial

---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+  bg: {
+    type: String,
+    default: 'bg-gray-100',
+  }
+});
+</script>
+
+<template>
+  <!-- remember backticks so you can type in js -->
+  <div :class="`${bg} p-6 rounded-lg shadow-md`">
+    <!-- slot is same as children in react -->
+    <slot></slot>
+  </div>
+</template>
---
+++ src/components/JobListings.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <h1>job listings</h1>
+</template>
