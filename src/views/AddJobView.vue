0b589cf0a79e46083e74483c722d319da952ecf0
griminir <viktor.elliot.king@gmail.com>
2025-02-11 10:15:58 +0100

    imported and added toast

--- src/views/AddJobView.vue
+++ src/views/AddJobView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { reactive } from 'vue';
 import axios from 'axios';
+import { useToast } from 'vue-toastification';
 import router from '@/router';
 
 const form = reactive({
@@ -16,6 +17,8 @@ const form = reactive({
     contactPhone: '',
   },
 });
+
+const toast = useToast();
 
 const handleSubmit = async () => {
   const newJob = {
@@ -35,8 +38,10 @@ const handleSubmit = async () => {
   try {
     const response = await axios.post(`/api/jobs`, newJob);
     router.push(`/jobs/${response.data.id}`);
+    toast.success('Job added successfully!');
   } catch (error) {
     console.error('Error adding job', error);
+    toast.error('Error adding job');
   }
 };
 </script>
